7c48de7fa941a53003ee7cc7d39f92335add621b
GW_MC <72297530+GWMCwing@users.noreply.github.com>
2023-09-29 13:12:38 +0800

    fix image does not follow parent width

--- src/components/App/Note/PageElement/PageElement.tsx
+++ src/components/App/Note/PageElement/PageElement.tsx
@@ -1,11 +1,14 @@
+import { forwardRef } from 'react';
 import style from './style.module.css';
 
-export default function PageElement({ children }: { children: JSX.Element }) {
+const PageElement = forwardRef(function ({ children }: { children: JSX.Element }, ref: React.Ref<HTMLDivElement>) {
   return (
-    <div className={`${style['page-element']} ${style['page-element_hoverable']}`}>
+    <div className={`${style['page-element']} ${style['page-element_hoverable']}`} ref={ref}>
       {/*  */}
       {children}
       {/*  */}
     </div>
   );
-}
+});
+
+export default PageElement;
--- src/components/App/Note/PageElement/image/image.tsx
+++ src/components/App/Note/PageElement/image/image.tsx
@@ -4,15 +4,21 @@ import { Resizable } from 're-resizable';
 import { useRef } from 'react';
 
 export default function PageElement_Image({ url, alt = '' }: { url: string; alt?: string }) {
+  const pageElementRef = useRef<HTMLDivElement>(null);
   const resizableRef = useRef<Resizable>(null);
   return (
-    <PageElement>
+    <PageElement ref={pageElementRef}>
       <Resizable
         lockAspectRatio={true}
         ref={resizableRef}
-        maxWidth={'90%'}
         style={{
           position: 'relative',
+        }}
+        onResize={(e, direction, ref, d) => {
+          resizableRef.current?.updateSize({
+            width: Math.min(ref.offsetWidth, pageElementRef.current?.offsetWidth ?? Infinity),
+            height: Math.min(ref.offsetHeight, pageElementRef.current?.offsetHeight ?? Infinity),
+          });
         }}
       >
         <Image
@@ -20,17 +26,30 @@ export default function PageElement_Image({ url, alt = '' }: { url: string; alt?
           src={url}
           width={0}
           height={0}
-          objectFit="contain"
+          objectFit="cover"
           sizes="100vw"
           style={{
-            height: '100%',
-            width: 'auto',
+            height: 'auto',
+            width: '100%',
             aspectRatio: 'auto',
           }}
           onLoadingComplete={(e) => {
-            resizableRef.current?.updateSize({
-              width: e.naturalWidth,
-              height: e.naturalHeight,
+            if (resizableRef.current === null) return;
+            if (pageElementRef.current === null) return;
+            const pe = pageElementRef.current;
+            const style = window.getComputedStyle(pe);
+            const paddingInline = parseFloat(style.paddingLeft) + parseFloat(style.paddingRight);
+            const paddingBlock = parseFloat(style.paddingTop) + parseFloat(style.paddingBottom);
+            const aspectRatio = e.naturalWidth / e.naturalHeight;
+            // const width = Math.min(e.width, pe.clientWidth - padding ?? Infinity);
+            // resizableRef.current.updateSize({
+            //   width: width,
+            //   height: width / aspectRatio,
+            // });
+            const height = Math.min(e.height, pe.clientHeight - paddingBlock ?? Infinity);
+            resizableRef.current.updateSize({
+              width: height * aspectRatio,
+              height: height,
             });
           }}
         />
